2aca76649aab59518ba2a9a1235f7a6762099145
Joel Huff <jh467510@ohio.edu>
2015-12-01 16:02:37 -0500

    Added large repository of faces, changed facial recognition code to use many more faces allowing for better facial recognition

--- FaceTrackTest/app/build/generated/source/r/debug/com/robodoot/dr/facetracktest/R.java
+++ FaceTrackTest/app/build/generated/source/r/debug/com/robodoot/dr/facetracktest/R.java
@@ -1625,10 +1625,10 @@ containing a value of this type.
         public static final int alertTitle=0x7f0d0031;
         public static final int always=0x7f0d001e;
         public static final int any=0x7f0d0013;
-        public static final int arrow_down=0x7f0d0053;
-        public static final int arrow_left=0x7f0d0055;
-        public static final int arrow_right=0x7f0d0054;
-        public static final int arrow_up=0x7f0d0052;
+        public static final int arrow_down=0x7f0d0052;
+        public static final int arrow_left=0x7f0d0053;
+        public static final int arrow_right=0x7f0d0055;
+        public static final int arrow_up=0x7f0d0054;
         public static final int back=0x7f0d0014;
         public static final int beginning=0x7f0d001b;
         public static final int buttonPanel=0x7f0d0037;
@@ -1788,10 +1788,8 @@ containing a value of this type.
         public static final int abc_toolbar_collapse_description=0x7f070010;
         public static final int action_settings=0x7f070012;
         public static final int app_name=0x7f070013;
-        public static final int hello_world=0x7f070014;
         public static final int status_bar_notification_info_overflow=0x7f070011;
-        public static final int title_activity_fd=0x7f070015;
-        public static final int title_activity_main=0x7f070016;
+        public static final int title_activity_fd=0x7f070014;
     }
     public static final class style {
         public static final int AlertDialog_AppCompat=0x7f09007a;
