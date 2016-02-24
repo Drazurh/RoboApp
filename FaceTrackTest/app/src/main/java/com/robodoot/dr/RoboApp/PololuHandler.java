4923124f482094926f8a529042343b058f4da7af
John Huff <jh571912@ohio.edu>
2016-02-24 00:57:14 -0500

    rolled back to previous version of PololuHandler

--- FaceTrackTest/app/src/main/java/com/robodoot/dr/RoboApp/PololuHandler.java
+++ FaceTrackTest/app/src/main/java/com/robodoot/dr/RoboApp/PololuHandler.java
@@ -9,53 +9,42 @@ import android.hardware.usb.UsbManager;
 import org.pololu.maestro.*;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
 /**
  * Created by Joel on 12/7/2015.
  */
-
-class Servo{
-
-    public int minPosition;
-    public int maxPosition;
-    public int position;
-    public int id;
-    MaestroSSC maestro;
-
-    public Servo(){
-        this.minPosition = 0;
-        this.maxPosition = 0;
-        this.position = -1;
-        this.id = -1;
-    }
-
-    public Servo(MaestroSSC _maestro, int _id, int _min, int _max){
-        this.id = _id;
-        this.minPosition = _min;
-        this.maxPosition = _max;
-        this.position = -1;
-        this.maestro = _maestro;
-
-    }
-
-    public void setPosition(int _position){
-        this.maestro.setTarget(this.id,_position);
-    }
-
-}
-
-
 public class PololuHandler implements Serializable {
 
-    MaestroSSC maestro;
+    public MaestroSSC maestro;
 
     public static final int NECK_YAW_SERVO = 10;
     public static final int NECK_PITCH_SERVO = 9;
+    private boolean isConnected=false;
 
-    int num_servos = 16;
+    public enum Motor{
+        HEAD_YAW        (9, "Head Yaw",1600,0,0),
+        HEAD_PITCH      (10, "Head Pitch",2200,0,0);
 
-    ArrayList<Servo> ServoList = new ArrayList<Servo>();
+
+        public final int number;
+        public final int homePos;
+        public final int min;
+        public final int max;
+        public final String name;
+
+
+
+        Motor(int num, String str, int h, int mi, int ma)
+        {
+            number = num;
+            name = str;
+            homePos = h;
+            min = mi;
+            max = ma;
+
+        }
+
+    }
 
 
 
@@ -66,14 +55,34 @@ public class PololuHandler implements Serializable {
     public int yaw=1600;
     public int pitch=2200;
 
-    public PololuHandler()
+    public void setTarget(int ID, int target)
     {
-        maestro = new MaestroSSC();
-        ServoList.add(new Servo(maestro, NECK_YAW_SERVO, 100, 200));
-        ServoList.add(new Servo(maestro, NECK_PITCH_SERVO, 100, 200));
+        if(isConnected)
+        {
+            maestro.setTarget(ID, target);
+
+        }
+
     }
 
 
+
+    public PololuHandler()
+    {
+
+        maestro = new MaestroSSC();
+
+
+
+
+
+    }
+
+    public boolean isOpen()
+    {
+        return isConnected;
+
+    }
 
     public void home()
     {
@@ -81,6 +90,9 @@ public class PololuHandler implements Serializable {
         maestro.setTarget(10,1600);
         yaw = 1600;
         pitch = 2200;
+
+
+
 
     }
 
@@ -92,12 +104,14 @@ public class PololuHandler implements Serializable {
 
     public void onResume(Intent intent,Activity parent ){
         String action = intent.getAction();
+        isConnected = false;
 
         if (action.equals("android.hardware.usb.action.USB_DEVICE_ATTACHED")) {
             UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
             if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
                 UsbManager usbManager = (UsbManager) parent.getSystemService(Context.USB_SERVICE);
                 maestro.setDevice(usbManager, device);
+                isConnected = true;
             } else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
                 maestro.setDevice(null, null);
             } else {
